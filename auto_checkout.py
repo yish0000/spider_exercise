aeb3d661764b5b991c46ffd5d89d9ac0cd92b703
Yish Shi <yish0000@foxmail.com>
2019-02-18 15:31:36 +0800

    实现签到和签退条件的判断

--- auto_checkout.py
+++ auto_checkout.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
@@ -21,18 +22,32 @@ def login(user, password):
     browser.find_element_by_id("login_user").send_keys(user)
     browser.find_element_by_id("login_pswd").send_keys(password)
     browser.find_element_by_id("login_btn").click()
-    WebDriverWait(browser, 3)
+    WebDriverWait(browser, 10)
+
+def checkin():
+    browser.execute_script("DoSignIn();")
+    WebDriverWait(browser, 10).until(
+        lambda x: x.find_element_by_class_name('fullscreenmask').is_displayed())
 
 def checkout():
     browser.execute_script("DoSignOutCheck();")
-    WebDriverWait(browser, 3).until(
+    WebDriverWait(browser, 10).until(
         lambda x: x.find_element_by_class_name('signOutBtn').is_displayed())
 
 def checkout_confirm():
     btn_confirm = browser.find_element_by_class_name('signOutBtn')
-    btn_confirm.click()
-    WebDriverWait(browser, 3).until(
-        lambda x: x.find_element_by_class_name('fullscreenmask').is_displayed())
+    if btn_confirm.is_displayed():
+        remain_time = browser.find_element_by_class_name('signMsgBox')
+        matchObj = re.match(r'.*当前时间：(.*):.*实际工作时长：(.*)小时.*', remain_time.text, re.DOTALL)
+        if matchObj:
+            if int(matchObj.group(1)) >= 18 and int(matchObj.group(2)) >= 8:
+                btn_confirm.click()
+                WebDriverWait(browser, 10).until(
+                    lambda x: x.find_element_by_class_name('fullscreenmask').is_displayed())        
+            else:
+                print("It's too early to checkout!!!")
+    else:
+        print("You can't checkout now!")
 
 def open_driver():
     global browser
@@ -49,9 +64,11 @@ def open_driver():
 def run():
     login(USER, PASSWD)
     browser.get(INDEX_URL)
-    checkout()
-    checkout_confirm()
-
+    if browser.find_element_by_class_name('signIn1').is_displayed():
+        checkin()
+    else:
+        checkout()
+        checkout_confirm()
 
 def main():
     open_driver()
