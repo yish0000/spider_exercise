25844c4e3bbc111ecc6dd3a7e4525c7cddfe0960
Yish Shi <yish0000@foxmail.com>
2019-02-20 20:06:47 +0800

    签到时间设置到21点之后，这样就能领加班餐了

--- checkout.bat
+++ checkout.bat
@@ -1,5 +1,5 @@
 @echo off
 
 set /a num=%RANDOM%%%60
-schtasks /Create /f /tn checkin /sc once /st 19:%num% /tr "D:\Project\python\auto_checkout.exe shizhenhua yish8866983"
+schtasks /Create /f /tn checkout /sc once /st 21:%num% /tr "D:\Project\python\auto_checkout.exe shizhenhua yish8866983"
 pause
