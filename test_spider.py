bfcee46d2394972186bf19edf4fe1bde911983f9
Yish Shi <yish0000@foxmail.com>
2019-02-22 11:35:44 +0800

    测试一下urllib

--- test_spider.py
+++ test_spider.py
@@ -1,12 +1,24 @@
 ﻿#测试一下爬虫
 
+import sys
 import re
-import requests
-import hashlib
-import time
+from urllib import request, parse
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ActionChains
+
+URL = 'http://web.oa.zulong.com/C6/ZuLongFlowModule.Web/Ajax/OvertimeMeal.aspx'
+DATA = b'<root><Data><Module>GetABData</Module><Date></Date></Data></root>'
+
+def http_post(url, data):
+	response = request.urlopen(url, data=data)
+	return response.read()
 
 def main():
-	print("hello world!")
+	resp = http_post(URL, DATA)
+	print(resp)
 
 if __name__ == '__main__':
 	main()
